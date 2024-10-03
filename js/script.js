415cbd2651737f4b29d2e866fbd84fdcdea62f69
Armadhani Hiro <hiroarmadhani@gmail.com>
2024-10-03 13:29:07 +0700

    add shopping cart and search button feature

--- js/script.js
+++ js/script.js
@@ -7,12 +7,40 @@ document.querySelector("#grocery-products").onclick = () => {
     navbarNav.classList.toggle("active");
 };
 
+// Toggle class active for search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active for shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
 // When click outside feature
-const bars = document.querySelector("#grocery-products");
-document.addEventListener("click", function (e) {
-    if (!bars.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
-    }
+const bars = document.querySelector('#grocery-products');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!bars.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
 
 document.querySelectorAll('.category-link').forEach(item => {
